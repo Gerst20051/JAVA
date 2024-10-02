662ce3c936b7ee50e8efca8238bb054e95b8bb61
Andrew Gerst <gerst20051@gmail.com>
2024-10-01 18:55:19 -0700

    add hello world example script

---
+++ examples/HelloWorld.java
@@ -0,0 +1,11 @@
+// Compiles HelloWorld.class
+// [$]> javac HelloWorld.java
+
+// Runs HelloWorld.class
+// [$]> java HelloWorld
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
